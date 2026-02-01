5d18e759837cc82d5aa5d08e468d03b57c78b74c
Omniladder <dustintobrien@gmail.com>
2026-02-01 05:13:51 +0000

    defaulting to catppuccin

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require "user.pckr"
 require "user.main"
 require "user.keymaps"
+vim.cmd.colorscheme("catppuccin")
---
+++ rebuild_script.sh
@@ -0,0 +1,5 @@
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
+
+sudo nixos-rebuild switch --flake ~/flakes#CatP   --override-input nvim-config path:/home/omniladder/Projects/nvim-config
